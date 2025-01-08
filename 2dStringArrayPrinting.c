f88e936bae80822f83ca6b4e238ac12427b97b10
Jayesh Singh Lodha <jayeshlodha177@gmail.com>
2025-01-08 17:24:30 +0530

    validation for input updated

--- 2dStringArrayPrinting.c
+++ 2dStringArrayPrinting.c
@@ -3,18 +3,49 @@
 
 char matrix[10][102][51];
 
-int take_input(char* input_message,int min_length,int max_length){
-    printf("%s",input_message);
-    int input;
-    while(1){
-        if(scanf("%d",&input)!=1 || input<min_length || input>max_length){
-            printf("Invalid input. Enter a number between %d & %d: ",min_length
-            ,max_length);
-            while (getchar() != '\n');
+int is_digit(char character) {
+    return character >= '0' && character <= '9';
+}
+
+int take_input(char *input_message, int min_value,int max_value) {
+    char input_array[100];
+    int is_valid;
+    int value = 0;
+
+    while (1) {
+        printf("%s", input_message);
+        scanf("%s", input_array);
+
+        is_valid = 1;
+        for (int i = 0; input_array[i] != '\0'; i++) {
+            if (!is_digit(input_array[i])) {
+                is_valid = 0;
+                break;
+            }
         }
-        else break;
+
+        if (is_valid) {
+            value = 0;
+            for (int i = 0; input_array[i] != '\0'; i++) {
+                value = value * 10 + (input_array[i] - '0');
+                if (value > max_value) {
+                    is_valid = 0;
+                    break;
+                }
+            }
+            if (value < min_value || value > max_value) {
+                is_valid = 0;
+            }
+        }
+
+        if (!is_valid) {
+            printf("Invalid input! Please enter a valid positive integer between %d and %lld.\n", min_value, max_value);
+        } else {
+            break;
+        }
     }
-    return input;
+
+    return value;
 }
 
 int is_vowel(char character){
@@ -24,14 +55,16 @@ int is_vowel(char character){
 }
 
 int is_alphabet(char character){
-    return character>='a' && character<='z';
+    return (character>='a' && character<='z')||
+     (character>='A' && character<='Z');
 }
 
-int check_valid_name(char* name){
+int check_name(char* name){
     while(*name!='\0'){
         if(!is_alphabet){
             return 0;
         }
+        name++;
     }
     return 1;
 }
@@ -43,6 +76,7 @@ void take_matrix_input(int rows,int columns,int *count
         for(int j=0;j<columns;j++){
             printf("Name at (%d %d):",i,j);
             scanf("%s",matrix[i][j]);
+            
             if(is_vowel(matrix[i][j][0])){
                 *count=*count+1;
                 if(strlen(matrix[i][j])>*size)*size=strlen(matrix[i][j]);
@@ -62,6 +96,10 @@ void display_matrix(int rows,int columns){
 }
 
 void display_longest_name_with_vowel(int rows,int columns,int size){
+    if(size==0){
+        printf("No names starts with a vowel\n");
+        return;
+    }
     printf("The longest name is: ");
     for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
